45b1c9985f6224f69e5549a92a987ab01b43ea14
Frzz23 <frenzdrez@gmail.com>
2024-09-13 18:56:42 +0545

    front page work

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -1,4 +1,6 @@
 import { FiFacebook, FiTwitter, FiInstagram, FiLinkedin } from "react-icons/fi";
+import { FaFacebookSquare } from "react-icons/fa";
+
 import Logo from "../../assets/Images/logo/logo9.png";
 import { NavLink } from "react-router-dom";
 import AppStore from "../../assets/Images/img/AppStore.png";
@@ -7,23 +9,23 @@ import hand from "../../assets/Images/img/hand.png";
 
 const Footer = () => {
   return (
-    <footer className="font-mono mt-10 p-6  max-w-full  bg-gray-100 shadow-md border " >
+    <footer className="font-mono mt-10 p-6  max-w-full  bg-stone-300 shadow-md border " >
       <div className="flex ">
       <div className=" flex justify-between  w-full  ">
       <div>
       <img src={Logo} alt="GundrukReads" className="h-36 mb-4 pl-16 " />
       <div className="flex space-x-6 mt-4    md:mt-0 pl-20">
           <a href="#" aria-label="Facebook" className="text-gray-400 hover:text-gray-300 transition-colors">
-            <FiFacebook className="h-8 w-8" />
+            <FaFacebookSquare  className="h-8 w-8 hover:text-gray-500 hover:bg-stone-300" />
           </a>
           <a href="#" aria-label="Twitter" className="text-gray-400 hover:text-gray-300 transition-colors">
-            <FiTwitter className="h-8 w-8" />
+            <FiTwitter className="h-8 w-8 hover:text-gray-500 hover:bg-stone-300" />
           </a>
           <a href="#" aria-label="Instagram" className="text-gray-400 hover:text-gray-300 transition-colors">
-            <FiInstagram className="h-8 w-8" />
+            <FiInstagram className="h-8 w-8 hover:text-gray-500 hover:bg-stone-300" />
           </a>
           <a href="#" aria-label="LinkedIn" className="text-gray-400 hover:text-gray-300 transition-colors">
-            <FiLinkedin className="h-8 w-8" />
+            <FiLinkedin className="h-8 w-8 hover:text-gray-500 hover:bg-stone-300 " />
           </a>
         </div>
       </div> 
@@ -32,7 +34,7 @@ const Footer = () => {
         <p className="font-bold text-[25px]  mb-2">Service</p>
         <div className="flex flex-col space-y-2 text-[17px] pl-0.5">
         <NavLink to="/gundrukreads-FAQ" className="no-underline  text-slate-700 hover:underline hover:text-red-500 ">FAQ</NavLink>
-        <a href="#"to="/gundrukreads-Motto" className="no-underline  text-slate-700 hover:underline hover:text-red-500">Contact Us</a>
+        <NavLink to="/gundrukreads-contactus" className="no-underline  text-slate-700 hover:underline hover:text-red-500">Contact Us</NavLink>
         <a href="#"to="/gundrukreads-Motto" className="no-underline  text-slate-700 hover:underline hover:text-red-500">Payment</a>
         <a href="#"to="/gundrukreads-Motto" className="no-underline  text-slate-700 hover:underline hover:text-red-500">Order</a>
         </div>
@@ -56,11 +58,13 @@ const Footer = () => {
         </div> */}
       </div>
       </div>
+      <div className="flex pr-28">
             <img src={hand} alt="" className="h-[14rem]  "/>
             <div className="space-y-4 mr-[24px] mt-10">
             <h3 className="text-[15px] w-max">Download It Here</h3>
             <img src={GooglePay} alt="" className="h-12" />
             <img src={AppStore} alt="" className="h-12" />
+      </div>
       </div>
       </div>
       <div className=" border-t border-gray-600 pt-7  text-center text-[15px] space-y-0.5 mt-2">
---
+++ src/pages/contactus/Contactus.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Bookstore from "../../assets/Images/img/Bookstore.png"
+const Contactus = () => {
+  return (
+    <>
+    <div className='flex items-center justify-center'>
+    <img src={Bookstore} alt="Bookstore" className='w-[83rem] h-60 pl-1' />
+    </div>
+    </>
+  )
+}
+
+export default Contactus
