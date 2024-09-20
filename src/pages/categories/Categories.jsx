f0f1e454453bc721546f8aee967446b9a613467a
Frzz23 <frenzdrez@gmail.com>
2024-09-20 19:31:19 +0545

    Categories_section

---
+++ src/pages/categories/Categories.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Categories = () => {
+  return (
+    <>
+    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 px-24 '>
+
+    </div>
+    </>
+  )
+}
+
+export default Categories
